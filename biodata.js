bdad22b2f09e7faad7af5e2a84df73d65703e020
lilyy21 <afrilialily2104@gmail.com>
2024-11-29 11:02:21 +0800

    Add files via upload

---
+++ biodata.js
@@ -0,0 +1,4 @@
+console.log("hello arif eni");
+alert("acccept cookies");
+var hasil = document.getElementById("hasil-output");
+hasil.innerHTML = " <h1> biodata </h1> <p>nama : karoline gwinneta babut </p> kelas : XI PPLG";
